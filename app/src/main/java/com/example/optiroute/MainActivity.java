96809478f66df605cd5cb1472432736309e5137d
eastha10 <kdh9158kdh@gmail.com>
2024-08-07 21:34:26 +0900

    앱 아이콘 완성도 100%, 홈화면 70%, 이외의 페이지 미완성

--- app/src/main/java/com/example/optiroute/MainActivity.java
+++ app/src/main/java/com/example/optiroute/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.maru);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
         Button cat1Button = findViewById(R.id.Cat1);
         Button cat2Button = findViewById(R.id.Cat2);
