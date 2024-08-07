a841b61340051a61d75bb57dbe396551a02fc88b
Tonyjoo11 <hyungjoo.ree@gmail.com>
2024-08-07 16:36:26 +0900

    APITest 추가

---
+++ app/src/androidTest/java/com/example/optiroute/APITest.java
@@ -0,0 +1,10 @@
+package com.example.optiroute;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+public class APITest {
+
+}
