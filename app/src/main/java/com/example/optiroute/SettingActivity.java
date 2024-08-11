e0657cb071fbb2e11b409a76b837a13107c23cd3
eastha10 <kdh9158kdh@gmail.com>
2024-08-11 23:16:00 +0900

    코드 실수 죄송합니다 ㅠㅠ

--- app/src/main/java/com/example/optiroute/SettingActivity.java
+++ app/src/main/java/com/example/optiroute/SettingActivity.java
@@ -1,4 +1,16 @@
 package com.example.optiroute;
 
-public class SettingActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_page);
+
+        // 여기에 SettingActivity의 로직을 추가합니다.
+    }
 }
+
